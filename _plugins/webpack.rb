4ad5a7c5d5fb84b6ee60875896d0c29442c36c6d
Anton Strogonoff <anton@strogonoff.name>
2018-05-03 15:26:37 +0700

    fix(plugins/webpack): infer theme root if no theme

--- _plugins/webpack.rb
+++ _plugins/webpack.rb
@@ -10,7 +10,12 @@ end
 Jekyll::Hooks.register :site, :post_write do |site|
   p "-- Eva: Building assets with Webpack…"
 
-  theme_dir = site.config["theme_root"] || site.theme.root
+  if site.theme or site.config["theme_root"]
+    theme_dir = site.config["theme_root"] || site.theme.root
+  else
+    theme_dir = site.source
+  end
+
   out_assets_dir = File.join(site.config["destination"], 'assets')
   cfg_path = site.config["webpack_config"] || File.join(theme_dir, 'webpack.config.js')
   system("npx webpack --config #{cfg_path} --context #{theme_dir} --output-path #{out_assets_dir}")
